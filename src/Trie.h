7f4f8e5807361cc35e7c1b4f069952eff7c7adde
Lalalaa236 <nhankiet236@gmail.com>
2023-06-30 00:37:28 +0700

    Added functions definition and deleted Trie.cpp

--- src/Trie.h
+++ src/Trie.h
@@ -5,6 +5,8 @@
 #include <string>
 
 using std::string;
+using std::cin;
+using std::cout;
 
 template<class Data> 
 class trieNode
@@ -28,11 +30,112 @@ class Trie
 
         trieNode<Data>* getRoot();// func to get root
         
-        bool insert(const string &str, const Data& data);
+        bool insert(const string &str); //, const Data& data);// insert data into trie
+        bool findWhole(const string &str);// find a word in the trie
 
     private:
         trieNode<Data>* root;
         void deallocate(trieNode<Data>* &root);
 };
+
+template<class Data>
+trieNode<Data>::trieNode()
+: data(), endOfWord(false)
+{
+    for(int i = 0; i < 94; ++i)
+    {
+        children[i] = nullptr;
+    }
+}
+
+template<class Data>
+trieNode<Data>::trieNode(Data data)
+: data(data), endOfWord(false)
+{
+    for(int i = 0; i < 94; ++i)
+    {
+        children[i] = nullptr;
+    }
+}
+
+template<class Data>
+Trie<Data>::Trie()
+: root(nullptr)
+{}
+
+template<class Data>
+Trie<Data>::~Trie()
+{
+    deallocate(this->root);
+    cout << "Deleted trie\n";
+}
+
+template<class Data>
+void Trie<Data>::deallocate(trieNode<Data>* &root)
+{
+    if(!root)
+        return;
+    
+    for(int i = 0; i < 94; ++i)
+    {
+        deallocate(root->children[i]);
+    }
+
+    delete root;
+    root = nullptr; 
+}
+
+template<class Data>
+bool Trie<Data>::insert(const string& str)//, const Data& data)
+{
+    if(!this->root)
+        this->root = new trieNode<Data>();
+    
+    trieNode<Data>* cur = this->root;
+
+    int length = str.length();
+    for(int i = 0; i < length; ++i)
+    {
+        int index = str[i] - '!';
+        
+        if(!cur->children[index])
+            cur->children[index] = new trieNode<Data>();
+
+        cur = cur->children[index]; 
+    }
+
+    if(!cur->endOfWord)
+    {
+        cur->endOfWord = true;
+        //cur->data = data;
+        return true;
+    }
+    
+    return false;
+}
+
+template<class Data>
+bool Trie<Data>::findWhole(const string& str)
+{
+    if(!this->root)
+        return false;
+    
+    trieNode<Data>* cur = this->root;
+    int length = str.length();
+
+    for(int i = 0; i < length; ++i)
+    {
+        int index = str[i] - '!';
+
+        if(!cur->children[index])
+            return false;
+        
+        cur = cur->children[index];
+    }
+
+    if(cur->endOfWord)
+        return true;
+    return false;
+}
 
 #endif 
